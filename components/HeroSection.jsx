d3ec063eceb0dfef7188c5b8322baee07fed7b7f
Gabriel Lebrun <lebrungabriel75@gmail.com>
2023-04-20 17:31:38 +0200

    home page modified

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,46 +3,46 @@ import { Image } from "@chakra-ui/react";
 
 const HeroSection = () => {
   return (
-    <section className="container mx-auto h-screen bg-red-200 flex justify-center ">
-      <div className="grid grid-cols-8 grid-rows-6 gap-2 w-full lg:w-10/12 h-full px-2 sm:px-0">
-        <div className="relative col-span-3 row-start-1 row-end-3">
+    <section className="container mx-auto h-screen flex justify-center ">
+      <div className="grid grid-cols-8 grid-rows-8 gap-2 w-full lg:w-10/12 h-full px-2 sm:px-0">
+        <div className="relative col-span-8 row-start-1 row-end-3">
           <Image
-            src="assets/lomepal.jpg"
-            alt="s"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="relative col-span-5 row-start-1 row-end-2 md:col-span-6">
-          <Image
-            src="assets/maria03.jpg"
-            alt="s"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="relative col-span-5 row-start-2 row-end-3 md:col-span-6">
-          <Image
-            src="assets/elio.jpg"
-            alt="s"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="relative col-span-4 row-start-3 row-end-5 md:col-start-4">
-          <Image
-            src="assets/anthony01.jpg"
+            src="assets/maria-screenshot.png"
             alt="s"
             className="h-full w-full object-cover"
           />
         </div>
         <div className="relative col-span-4 row-start-3 row-end-5">
           <Image
-            src="assets/street.jpg"
+            src="assets/lomepal.jpg"
             alt="s"
             className="h-full w-full object-cover"
           />
         </div>
-        <div className="relative col-span-8 row-start-5 row-end-7">
+        <div className="relative col-span-4 row-start-3 row-end-4">
           <Image
-            src="assets/neyt.jpg"
+            src="assets/elio-screenshot.jpg"
+            alt="s"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="relative col-span-4 row-start-4 row-end-5">
+          <Image
+            src="assets/chair.jpg"
+            alt="s"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="relative col-span-5 row-start-5 row-end-8">
+          <Image
+            src="assets/manif02.jpg"
+            alt="s"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="relative col-span-3 row-start-5 row-end-8">
+          <Image
+            src="assets/yassine01.jpg"
             alt="s"
             className="h-full w-full object-cover"
           />
