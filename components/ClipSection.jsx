8cf35de761faf98b20d7010a5f8c0c839107b215
Gabriel Lebrun <lebrungabriel75@gmail.com>
2023-06-14 21:35:23 +0200

    clips added

--- components/ClipSection.jsx
+++ components/ClipSection.jsx
@@ -4,6 +4,8 @@ import YoutubeEmbed from "./YoutubeEmbed";
 const ClipSection = () => {
   return (
     <section className="container mx-auto grid grid-cols-1 gap-y-8 place-items-center">
+      <YoutubeEmbed embedId="RkpBVk1kzyA" />
+      <YoutubeEmbed embedId="3uILi_CpyM4" />
       <YoutubeEmbed embedId="raa6D6X2-oc" />
       <YoutubeEmbed embedId="ImLbCHTEcOc" />
       <YoutubeEmbed embedId="xs4CV02kDF4" />
